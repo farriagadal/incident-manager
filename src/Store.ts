6593815706c752248acfcf180cfc490c1f95c5db
Fernando <farriagadal94@gmail.com>
2023-04-10 15:54:38 -0400

    feat: added method "indicent_status"

--- src/Store.ts
+++ src/Store.ts
@@ -10,4 +10,34 @@ class Store {
     this.incidents.push(incident);
     return incident;
   }
+
+  
+  incident_status(startDate: Date, endDate: Date) {  // Método para calcular estadísticas de incidentes entre dos fechas
+    let openCases = 0;
+    let closedCases = 0;
+    let totalSolutionTime = 0;
+    let maximumSolution = 0;
+
+    for (const incident of this.incidents) {
+      if (incident.createdAt >= startDate && incident.createdAt <= endDate) {
+        if (incident.status === 'open') {
+          openCases++;
+          const solutionTime = (new Date().getTime() - incident.createdAt.getTime()) / 1000;
+          maximumSolution = Math.max(maximumSolution, solutionTime);
+        } else {
+          closedCases++;
+          const solutionTime = incident.getSolutionTime() as number;
+          totalSolutionTime += solutionTime;
+          maximumSolution = Math.max(maximumSolution, solutionTime);
+        }
+      }
+    }
+
+    return {
+      open_cases: openCases,
+      closed_cases: closedCases,
+      average_solution: closedCases > 0 ? totalSolutionTime / closedCases : 0,
+      maximum_solution: maximumSolution,
+    };
+  }
 }
